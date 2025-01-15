5ec4075499170c7180a80ff1544d87b9faa8b68e
Avi <avipars@users.noreply.github.com>
2025-01-15 13:18:40 +0200

    Update hashcat.bat

--- hashcat.bat
+++ hashcat.bat
@@ -1,5 +1,6 @@
 @echo off 
 REM where this is the directory of hashcat
+REM useful for when we are running hashcat and are pointing the SRC/DEST files to a folder on a different windows drive
 pushd "C:\Program Files\hashcat"
 hashcat.exe %*
 popd
