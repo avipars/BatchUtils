432d048baca5ba82f44e0d22ba8bdc6d5b534752
Avi <avipars@users.noreply.github.com>
2025-02-21 14:54:16 +0200

    Update winget_upgrade.bat

--- winget_upgrade.bat
+++ winget_upgrade.bat
@@ -2,7 +2,8 @@
 echo Starting the app upgrade process...
 
 REM -h for 
-winget upgrade --all -h
+winget upgrade --all -h 
+REM h is for silent install, all is to upgrade all packages available
 echo Upgrade process completed.
 
 cmd /k
